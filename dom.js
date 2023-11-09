1fa0927d2f7a7f0c172383171b3871e6e015e1da
amrv23 <148378100+amrv23@users.noreply.github.com>
2023-11-09 19:23:35 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,68 @@
+//console.log(document.getElementById('display'));
+//var inputbox = document.getElementById('display');
+//var ops = document.getElementsByClassName('operator');
+//var but =document.querySelector('.buttons');
+//console.log(but.parentNode);
+//but.parentNode.style.backgroundColor = '#000000';
+//var btn = document.querySelector('button');
+//let x = btn.innerHTML
+//alert(x)//
+
+var display = document.getElementById('display');
+
+let displayText = ""
+
+
+/*var numbers = document.querySelectorAll('button.numbers');
+
+/*for (let i = 0; i< numbers.length; i++){
+    var btn = numbers[i]
+    console.log(numbers[i]);
+    btn.addEventListener('click', numClicked());}
+
+function numClicked(q){
+    displayText+=q.innerHTML
+    display.value = displayText
+
+}
+numbers[0].addEventListener('click', numClicked(numbers[0]));
+numbers[1].addEventListener('click', numClicked(numbers[1]));
+numbers[2].addEventListener('click', numClicked(numbers[2]));
+numbers[3].addEventListener('click', numClicked(numbers[3]));
+numbers[4].addEventListener('click', numClicked(numbers[4]));
+numbers[5].addEventListener('click', numClicked(numbers[5]));
+numbers[6].addEventListener('click', numClicked(numbers[6]));
+numbers[7].addEventListener('click', numClicked(numbers[7]));
+numbers[8].addEventListener('click', numClicked(numbers[8]));
+numbers[9].addEventListener('click', numClicked(numbers[9]));*/
+
+document.body.addEventListener('click', function(event)
+{
+    if(event.target.classList.contains('numbers') || event.target.classList.contains('operator')){
+    displayText+=event.target.textContent
+    display.value = displayText
+    }
+
+    else if(event.target.classList.contains('AC')){
+        displayText = ""
+        display.value = displayText
+    }
+
+    else if(event.target.classList.contains('DEL')){
+        displayText = displayText.slice(0,-1)
+        display.value = displayText
+
+    }
+
+    else if(event.target.classList.contains('equals')){
+        displayText = eval(displayText)
+        display.value = displayText
+
+    }
+
+    
+    
+}    )
+
+   
+    
